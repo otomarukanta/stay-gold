3c70bd1ff1833068a914fa3f205abd4642310262
otomarukanta <kanta208@gmail.com>
2016-09-11 22:35:23 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='staygold',
+    version='0.0.1',
+    description='staygold',
+    author='otomarukanta',
+    author_email='kanta208@gmail.com',
+    url='http://otomarukanta.com/',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+        ]
+    )
